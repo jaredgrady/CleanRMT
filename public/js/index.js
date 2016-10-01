50b7cea6ce775ea31e2c394d4cf77aa3e931d086
Caleb Young <kierenninjo@yahoo.com>
2016-10-01 09:29:16 -0700

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,73 @@
 "use strict";
 
-const dex = require('./dex');
+const dex = require('./dex.js').dex;
+
+const megas = {
+	"abomasnow": "abomasite",
+	"absol": "absolite",
+	"aerodactyl": "aerodactylite",
+	"aggron": "aggronite",
+	"alakazam": "alakanite",
+	"altaria": "altarianite",
+	"ampharos": "ampharosite",
+	"audino": "audinite",
+	"banette": "banettite",
+	"beedrill": "beedrillite",
+	"blastoise": "blastoisinite",
+	"blaziken": "blazikenite",
+	"camerupt": "camperuptite",
+	"charizard": ["charizardite x", "charizardite y"],
+	"diancie": "diancite",
+	"gallade": "galladite",
+	"garchomp": "garchompite",
+	"gardevoir": "gardevoirite",
+	"gengar": "gengarite",
+	"glalie": "glaitite",
+	"gyarados": "gyaradosite",
+	"heracross": "heracronite",
+	"houndoom": "houndoominite",
+	"kangaskhan": "kangaskhanite",
+	"latios": "latiosite",
+	"latias": "latiasite",
+	"lopunny": "lopunnite",
+	"lucario": "lucarionite",
+	"manectric": "manectite",
+	"mawile": "mawilite",
+	"medicham": "medichamite",
+	"metagross": "metagrossite",
+	"mewtwo": ["mewtwonite x", "mewtwonite y"],
+	"pidgeot": "pidgeotite",
+	"pinsir": "pinsirite",
+	"sableye": "sablenite",
+	"salamence": "salamencite",
+	"sceptile": "sceptilite",
+	"scizor": "scizorite",
+	"sharpedo": "sharpedonite",
+	"slowbro": "slowbronite",
+	"steelix": "steelixite",
+	"swampert": "swampertite",
+	"tyranitar": "tyranitarite",
+	"venusaur": "venusaurite"
+};
+
 const formats = {
 	"pokesho": ["http://www.pokestadium.com/assets/img/sprites/misc/pokesho/", ".gif"],
 	"icons": ["http://www.pokestadium.com/assets/img/sprites/misc/icons/", ".png"],
-	"shuffle": ["http://media.pldh.net/pokemon/shuffle/", ".png", true],
+	"shuffle": ["http://www.pkparaiso.com/imagenes/shuffle/sprites/", ".png", "paraiso"],
 	"xyanimated": ["http://play.pokemonshowdown.com/sprites/xyani/", ".gif"],
 	"xy": ["http://play.pokemonshowdown.com/sprites/xy/", ".png"],
-	"smd": ["http://www.serebii.net/supermysterydungeon/pokemon/", ".png", true],
+	"smd": ["http://www.serebii.net/supermysterydungeon/pokemon/", ".png", "serebii"],
 };
 
-function hash (pokemon) {//gets dex number. eventually add support for megas
+function hash (pokemon, format) {//gets dex number. eventually add support for megas
+	pokemon = pokemon[0].toUpperCase() + pokemon.substr(1);
 	if (dex[pokemon]) return dex[pokemon];
 	if (~pokemon.indexOf("-")) {
-		return dex[pokemon.substr(0, pokemon.indexOf("-")).trim()];
+		let data = dex[pokemon.substr(0, pokemon.indexOf("-")).trim()];
+		console.log(pokemon[pokemon.indexOf("-") + 1]);
+		if (format === "serebii") data += "-" + pokemon[pokemon.indexOf("-") + 1];
+		if (format === "paraiso" && ~pokemon.indexOf("mega")) data += "-mega"; //todo: add support for formes
+		return data;
 	}
 	return pokemon;
 }
@@ -31,11 +85,13 @@ function getImgs (format, team) {
 function getImg (format, pokemon) {
 	if (!formats[format]) return;
 	let fdata= formats[format];
-	let output = '[img]' + fdata[0] + (fdata[2] ? hash(pokemon) : pokemon) + fdata[1] + '[/img]';
+	let output = '[img]' + fdata[0] + (fdata[2] ? hash(pokemon, fdata[2]) : pokemon) + fdata[1] + '[/img]';
 	return output;
 }
-function buildingProcess (format, pokemon) {
+
+function buildingProcess (format, pokemon, f, fe) {
 	let output = ["","","","","",""];
+	console.log(output);
 	for (let i = 0; i < pokemon.length; i++) {
 		let img = getImg(format, pokemon[i].toLowerCase());
 		for (let j = i; j < pokemon.length; j++) {
@@ -43,9 +99,9 @@ function buildingProcess (format, pokemon) {
 		}
 	}
 	for (let k = 0; k < output.length; k++) {
-		output[k] += "\n**Why did you choose this pokemon?**\n";
+		output[k] += "\n" + f + "**Why did you choose this pokemon?**" + fe + "\n";
 	}
-	return output.join();
+	return output.join("");
 }
 
 function removeNick (line) {
@@ -56,26 +112,117 @@ function removeNick (line) {
 	return line;
 }
 
+function checkMega (line) {
+	let pokemon = line[0].toLowerCase();
+	let item = line[1].toLowerCase().trim();
+	console.log(line[0])
+	if (!megas[pokemon]) return line[0];
+	if (megas[pokemon]) {
+		if (typeof(megas[pokemon]) !== "object" && megas[pokemon] === item) {
+			return line[0] + '-Mega';
+		} else if (typeof(megas[pokemon]) === "object") {
+			if (megas[pokemon][0] === item) return line[0] + '-Mega-X';
+			else if (megas[pokemon][1] === item) return line[0] + '-Mega-Y';
+		} else {
+			return line[0];
+		}
+	}
+}
+
+function toTitle (text, options) { //This function is a disaster lol
+	let output = "[" + options.align + "][FONT=" + options.tfont + "][SIZE=" + options.size + "]";
+	if (options.bold) output += "[B]";
+	if (options.underlined) output += "[U]";
+	if (options.bold === "first") text = text[0] + "[/B]" + text.substr(1);
+	output += text;
+	if (options.bold && options.bold !== "first") output += "[/B]";
+	if (options.underlined) output += "[/U]";
+	output += "[/SIZE][/FONT][/" + options.align + "]";
+	return output;
+}
+
 function rmt (team, options) {
 	let data = packTeam(team);
-	let output = "[center]" + getImgs(options.imgFormat, data) + "[/center]\n\n" +
-		"[SIZE=6]Introduction[/SIZE]\n\n**Introduction goes here**\n\n";
-	if (options.process) output+= "[SIZE=6]Teambuilding Process[/SIZE]\n\n" +
-		buildingProcess(options.processFormat, data.pokemon) + "\n" +
-		"\n[SIZE=6]The Team[/SIZE]\n\n";
-	for (let i = 0; i < data.pokemon.length; i++) {
-		output += getImg(options.imgFormat, data.pokemon[i].toLowerCase()) + "\n" +
-			data.pokenames[i] + " @ " + data.items[i] + "\n" +
-			"Ability: " + data.abilities[i] + "\n" +
-			"EVs: " + data.evs[i] + "\n" +
-			data.natures[i] + " Nature \n";
-		for (let k = 0; k < data.moves[i].length; k++) {
-			output += "- " + data.moves[i][k] + "\n";
+	let f = "[FONT=" + options.font + "]";
+	let fe = "[/FONT]";
+	let output = "";
+	output += "[center]" + getImgs(options.imgFormat, data) + "[/center]\n\n" +
+		toTitle("Introduction", options) + "\n\n" + f + "**Introduction goes here**" + fe + "\n\n";
+	if (options.process) output+= toTitle("Teambuilding Process", options) + "\n\n" + 
+		"[hide]" + buildingProcess(options.processFormat, data.pokemon, f, fe) + "[/hide]\n" +
+		"\n" +  toTitle("The Team", options) + "\n\n";
+	output += buildSets(data, options, f, fe);
+	output += toTitle("Conclusion", options) + "\n\n" + f + "**Conclusion goes here**" + fe + "\n\n" +
+		"[hide=Importable]" + data.importable + "[/hide]";
+	return output;
+}
+
+function buildSets (data, options, f, fe) {
+	let output = "";
+	console.log(options.setFormat);
+	switch (options.setFormat) {
+	case 0: //default
+		for (let i = 0; i < data.pokemon.length; i++) {
+			output += "[" + options. align + "]" + getImg(options.imgFormat, data.pokemon[i].toLowerCase()) + "\n" + 
+				data.pokenames[i] + " @ " + data.items[i] + "\n" +
+				"Ability: " + data.abilities[i] + "\n" +
+				"EVs: " + data.evs[i] + "\n" +
+				data.natures[i] + " Nature \n" +
+				(data.ivs[i] ? "IVs: " + data.ivs[i] + "\n" : "");
+			for (let k = 0; k < data.moves[i].length; k++) {
+				output += "- " + data.moves[i][k] + "\n";
+			}
+		output += "[/" + options.align + "]\n" + f + "" + f + "**Why did you choose this pokemon? What does it do for your team?**" + fe + "" + fe + "\n\n";
 		}
-		output += "\n**Why did you choose this pokemon? What does it do for your team?**\n\n";
+	break;
+	case 1: //default with bold
+		for (let i = 0; i < data.pokemon.length; i++) {
+			output += "[LEFT]" + getImg(options.imgFormat, data.pokemon[i].toLowerCase()) + "\n" + 
+				"[B]" + data.pokenames[i] + "[/B]" + (data.items[i] ? " @ " + data.items[i] : "") + "\n" +
+				"[B]Ability:[/B] " + data.abilities[i] + "\n" +
+				"[B]Nature:[/B] " + data.natures[i] + "\n" +
+				"[B]EVs:[/B] " + data.evs[i] + "\n" +
+				(data.ivs[i] ? "[B]IVs:[/B] " + data.ivs[i] + "\n" : "") +
+				"[B]Moves:[/B] ";
+			for (let k = 0; k < data.moves[i].length; k++) {
+				if (k !== 0) output+= " | ";
+				output += data.moves[i][k];
+			}
+		output += "[/LEFT]\n\n" + f + "**Why did you choose this pokemon? What does it do for your team?**" + fe + "\n\n";
+		}
+	break;
+	case 2: //trinitrotoluene
+		for (let i = 0; i < data.pokemon.length; i++) {
+			output += "[B][SIZE=2][LEFT][INDENT]" + getImg(options.imgFormat, data.pokemon[i].toLowerCase()) + "[/INDENT]\n" + 
+				data.pokenames[i] + (data.items[i] ? " @ " + data.items[i] : "") + " | " + 
+				data.abilities[i] + "\n" +
+				data.natures[i] + " | " +
+				data.evs[i] + "\n" +
+				(data.ivs[i] ? "IVs: " + data.ivs[i] + "\n" : "");
+			for (let k = 0; k < data.moves[i].length; k++) {
+				output += "• " + data.moves[i][k] + "\n";
+			}
+		output += "[/B][/SIZE][/LEFT]" + f + "**Why did you choose this pokemon? What does it do for your team?**" + fe + "\n\n";
+		}
+	break;
+	case 3: //Pearl
+		for (let i = 0; i < data.pokemon.length; i++) {
+			output += "[SIZE=2][CENTER]" + getImg(options.imgFormat, data.pokemon[i].toLowerCase()) + "\n" + 
+				(data.items[i] ? 
+					"@ [IMG]http://www.serebii.net/itemdex/sprites/" + data.items[i].toLowerCase().split(" ").join("") + ".png[/IMG]" : 
+					"") + "\n" +
+				"[B]Ability-[/B] " + data.abilities[i] + "\n" +
+				"[B]EVs-[/B] " + data.evs[i].split(' / ').join(' | ') + "\n" +
+				(data.ivs[i] ? "[B]IVs-[/B] " + data.ivs[i] + "\n" : "") +
+				"[B]Nature- [/B]" + data.natures[i] + "\n\n";
+			for (let k = 0; k < data.moves[i].length; k++) {
+				if (k !== 0) output+= " | ";
+				output += data.moves[i][k];
+			}
+		output += "[/SIZE][/CENTER]\n" + f + "**Why did you choose this pokemon? What does it do for your team?**" + fe + "\n\n";
+		}
+	break;
 	}
-	output += "[SIZE=6]Conclusion[/SIZE]\n\n**Conclusion goes here**\n\n" +
-		"[hide=importable]" + data.importable + "[/hide]";
 	return output;
 }
 
@@ -87,7 +234,8 @@ function packTeam (importable) {
 		"evs": [],
 		"abilities": [],
 		"natures": [],
-		"moves": []
+		"moves": [],
+		"ivs": []
 	};
 	team.importable = importable;
 	let lines = importable.split('\n');
@@ -111,14 +259,19 @@ function packTeam (importable) {
 				if (pokeno > team.evs.length) team.evs.push("No EVs");
 				if (pokeno > team.abilities.length) team.abilities.push("No Ability");
 				if (pokeno > team.natures.length) team.natures.push("No Nature");
+				if (pokeno > team.ivs.length) team.ivs.push(false);
 				let line = l.split(" @ ");
-				team.pokemon.push(removeNick(line[0]));
 				team.pokenames.push(line[0]);
-				line[1] ? team.items.push(line[1].trim()) : team.items.push("No Item");
+				//team.pokemon.push();
+				line [0] = removeNick(line[0]);
+				team.pokemon.push(checkMega(line));
+				line[1] ? team.items.push(line[1].trim()) : team.items.push(false);
 			} else if (~l.indexOf("EVs: ")) {
 				team.evs.push(l.substr(l.indexOf(' ')).trim());
 			} else if (~l.indexOf("Ability: ")) {
 				team.abilities.push(l.substr(l.indexOf(' ')).trim());
+			} else if (~l.indexOf("IVs: ")) {
+				team.ivs.push(l.substr(l.indexOf(' ')).trim());
 			} else if (~l.indexOf("Nature")) {
 				team.natures.push(l.substr(0, l.indexOf(' ')).trim());
 			} else if (~l.indexOf("- ")) {
@@ -129,9 +282,14 @@ function packTeam (importable) {
 					team.moves.push([l.substr(l.indexOf(" ")).trim()]);
 				}
 			}
+			let pokeno = team.pokemon.length;
+			if (pokeno > team.evs.length) team.evs.push("No EVs");
+			if (pokeno > team.abilities.length) team.abilities.push("No Ability");
+			if (pokeno > team.natures.length) team.natures.push("No Nature");
+			if (pokeno > team.ivs.length) team.ivs.push(false);
 		}
 	}
 	return team;
 }
 
-module.exports.rmt = rmt;
+module.exports.rmt = rmt; //fender sucks
