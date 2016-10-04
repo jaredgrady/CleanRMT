a1b8b13d5c957e99fb6615ca3f1b80705951f039
Jared Grady <jared@thekards.com>
2016-10-04 15:42:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const favicon = require('serve-favicon');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}));
