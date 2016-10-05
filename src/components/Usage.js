750a5a0eeb901d57042785ea3e5175c523c70c06
thefenderstory <jgrady15@brophybroncos.org>
2016-10-04 17:07:34 -0700

    Navbar style and usage message updates

--- src/components/Usage.js
+++ src/components/Usage.js
@@ -5,8 +5,8 @@ var Usage = React.createClass({
     return (
         <div id="usage">
          <p>This tool creates a template for Smogon Rate My Team (RMT) Posts. Copy and paste the
-            importable of your team into the entry box, select the image type you want from the
-            editor and click submit!
+            importable of your team into the entry box, select the options you want from the
+            menus and click submit!
          </p>
         </div>
     );
